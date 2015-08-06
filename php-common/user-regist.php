cd8f5cf2bbfaf8c5ea5000516c8fba5cfd7569f2
lijinyao <lijinyao@outlook.com>
2015-08-06 10:10:00 +0800

    add php-common

---
+++ php-common/user-regist.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lijin
+ * Date: 2015/8/5
+ * Time: 21:34
+ */
+require "sqlHelper.php";
+$username = $_POST['username'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$check = "SELECT * FROM user WHERE username='$username'";
+$usernameExist = $mysql->query($check)->fetch_array();
+if($usernameExist) {
+    echo 'user-exist';
+}else{
+    $sql = "INSERT INTO user(username, password, email, avatar) VALUES ('$username', '$password', '$email', 'default')";
+    $mysql->query($sql);
+}
